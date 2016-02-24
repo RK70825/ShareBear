b97ff516407a7b10a14435a747b6b86590df330e
Ross Kleiman <Ross Kleiman>
2016-02-24 14:23:01 -0600

    Initial commit

---
+++ app/src/main/java/com/kleiman/sharebear/MainActivity.java
@@ -0,0 +1,50 @@
+package com.kleiman.sharebear;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usernameInput;
+    EditText passwordInput;
+    TextView rossText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameInput = (EditText) findViewById(R.id.usernameInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        rossText = (TextView) findViewById(R.id.rossText);
+
+    }
+
+    //Save the user's login info
+    public void saveInfo(View view){
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("username", usernameInput.getText().toString());
+        editor.putString("password", passwordInput.getText().toString());
+        editor.apply();
+
+        Toast.makeText(this, "saved", Toast.LENGTH_LONG).show();
+    }
+
+    //Display the user's login info
+    public void displayData(View view){
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        String name = sharedPref.getString("username", "");
+        String pw = sharedPref.getString("password", "");
+
+        rossText.setText(name + " " + pw);
+    }
+}
